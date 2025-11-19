bc803e8a38933f7a263b48ef7eb7ddf57d005730
古川康介 <2311140050x@ed.fuk.kindai.ac.jp>
2025-11-19 08:55:32 +0000

    model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User', optional: true
+  has_many :participations, dependent: :destroy
+  has_many :users, through: :participations
+
+  validates :title, presence: true
+
+  def spots_left
+    return nil if capacity.nil?
+    capacity - participations.where(status: [0,1]).count
+  end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,2 +1,8 @@
 class Participation < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+
+  enum status: { provisional: 0, confirmed: 1, cancelled: 2 }
+
+  validates :user_id, uniqueness: { scope: :event_id, message: "は既にこのイベントに申込済みです" }
 end
