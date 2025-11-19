07c424ee209daa8f37be8739030e8a9182c44ee7
Yuuki Karakawa <2311140008k@ed.fuk.kindai.ac.jp>
2025-11-19 11:44:22 +0000

    userの新規追加の作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  
+  enum role: { student: 0, admin: 1 }
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+end
