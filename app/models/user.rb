d67ca0b8c481380e108343e1332a77fc40d0e8f5
Yuuki Karakawa <2311140008k@ed.fuk.kindai.ac.jp>
2025-11-26 06:43:09 +0000

    エラー解決

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   
-  enum role: { student: 0, admin: 1 }
+  enum :role, { student: 0, admin: 1 }
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
