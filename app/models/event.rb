ac0581a109670b8c5dd50244493a593c1808ef71
Yuuki Karakawa <2311140008k@ed.fuk.kindai.ac.jp>
2025-12-17 06:18:12 +0000

    Y3

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,21 @@ class Event < ApplicationRecord
 
   validates :title, :location, :start_datetime, :end_datetime, presence: true
   validates :capacity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  def participants_count
+    participations.count
+  end
+  
+  def remaining_capacity
+    capacity - participants_count
+  end
+  
+  def full?
+    remaining_capacity <= 0
+  end
+  
+  def participating?(user)
+    return false unless user
+    participations.exists?(user_id: user.id)
+  end
 end
